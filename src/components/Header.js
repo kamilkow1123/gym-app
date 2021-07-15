1be96d725d40444dceb4fa51ed5ff7b1b38081cc
kamilkow1123 <22kamil.kow@gmail.com>
2021-07-15 21:07:47 +0200

    Changing buttons in navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,75 +1,82 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import '../style/header.css';
-import useWindowSize from '../hooks/useWindowSize';
-import { CSSTransition } from 'react-transition-group';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import "../style/header.css";
+import useWindowSize from "../hooks/useWindowSize";
+import { CSSTransition } from "react-transition-group";
 
 const Header = () => {
-	const [ showMobileMenu, setShowMobileMenu ] = useState(false);
-	const { width } = useWindowSize();
+  const [ showMobileMenu, setShowMobileMenu ] = useState(false);
+  const { width } = useWindowSize();
 
-	let menu;
-	let hamburger;
-	let mobileMenu;
-	let mobileMenuMask;
+  let menu;
+  let hamburger;
+  let mobileMenu;
+  let mobileMenuMask;
 
-	if (width >= 768) {
-		menu = (
-			<div className="nav-menu">
-				<Link to="/login" className="item">
-					Trainer
-				</Link>
-				<Link to="/login" className="item">
-					Employee
-				</Link>
-				<Link to="/login" className="item">
-					Client
-				</Link>
-			</div>
-		);
-	} else {
-		hamburger = (
-			<div className="toggle-button">
-				<FontAwesomeIcon icon={faBars} onClick={() => setShowMobileMenu(!showMobileMenu)} />
-			</div>
-		);
+  if (width >= 768) {
+    menu = (
+      <div className="nav-menu">
+        <Link to="/login" className="item">
+          Login
+        </Link>
+        <Link to="/register" className="item">
+          Sign Up
+        </Link>
+      </div>
+    );
+  } else {
+    hamburger = (
+      <div className="toggle-button">
+        <FontAwesomeIcon
+          icon={faBars}
+          onClick={() => setShowMobileMenu(!showMobileMenu)}
+        />
+      </div>
+    );
 
-		mobileMenu = (
-			<div className="menu">
-				<CSSTransition in={showMobileMenu} unmountOnExit timeout={500} classNames="mobile-menu">
-					<div className="nav-mobile-menu">
-						<Link to="/login" className="item-mobile">
-							Trainer
-						</Link>
-						<Link to="/login" className="item-mobile">
-							Employee
-						</Link>
-						<Link to="/login" className="item-mobile">
-							Client
-						</Link>
-					</div>
-				</CSSTransition>
-			</div>
-		);
+    mobileMenu = (
+      <div className="menu">
+        <CSSTransition
+          in={showMobileMenu}
+          unmountOnExit
+          timeout={500}
+          classNames="mobile-menu"
+        >
+          <div className="nav-mobile-menu">
+            <Link to="/login" className="item-mobile">
+              Login
+            </Link>
+            <Link to="/register" className="item-mobile">
+              Sign Up
+            </Link>
+          </div>
+        </CSSTransition>
+      </div>
+    );
 
-		if (showMobileMenu) {
-			mobileMenuMask = <div className="mobile-menu-mask" onClick={() => setShowMobileMenu(false)} />;
-		}
-	}
+    if (showMobileMenu) {
+      mobileMenuMask = (
+        <div
+          className="mobile-menu-mask"
+          onClick={() => setShowMobileMenu(false)}
+        />
+      );
+    }
+  }
 
-	return (
-		<header className="navbar">
-			<Link to="/" className="item">
-				GymFit
-			</Link>
-			{mobileMenuMask}
-			{mobileMenu}
-			{hamburger}
-			{menu}
-		</header>
-	);
+  return (
+    <header className="navbar">
+      <Link to="/" className="item">
+        GymFit
+      </Link>
+      {mobileMenuMask}
+      {mobileMenu}
+      {hamburger}
+      {menu}
+    </header>
+  );
 };
 
 export default Header;
